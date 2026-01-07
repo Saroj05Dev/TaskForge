fa0ac90133ec3ba15a20bdc4640433f220d22435
Saroj_Dev <sarojtech0005@gmail.com>
2026-01-07 13:11:41 +0530

    Removed hardcoding from socket.js

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3200"
+const SOCKET_URL = import.meta.env.VITE_API_BASE_URL
 
 export const socket = io(SOCKET_URL, {
     withCredentials: true,
